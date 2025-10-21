742879413cfc52d8637b29092ba8b544dc570cce
CesarSotoPolo <henrysoto10@gmail.com>
2025-10-20 21:03:33 -0500

    Cambio 201025-02

--- 04-fundamentos js/07-objetos.js
+++ 04-fundamentos js/07-objetos.js
@@ -25,6 +25,8 @@ let perro2 = {
     edad: 18,
     color: "Blanco/Negro",
     amigable:true,
+    "vacunado-2025":true,
+    "tiene-chip":"Si tiene, 1234567",
     dormir(){
         console.log("zzzz")
     }
@@ -34,10 +36,48 @@ console.log(perro);
 
 console.table(perro2);
 
-console.log(perro.nombre);
+//comno acceder a propiedades
+console.log("Nombre:",perro.nombre); // sintaxis de punto
+
+let nombrePropiedad = "amigable";
+
+console.log("Amigable:", perro2[nombrePropiedad]); //sintaxis de corchete
+console.log("Vacunado:", perro2["vacunado-2025"]); 
+
+
+//Desestructuracion, aplicamos para no repetir perro. perro.
+//pero necesitamos saber los nombres de las propiedades
+let {  edad, color, nombre} = perro;
+console.log(edad);
+console.log(color);
+console.log(nombre);
+
+let {  edad:edad2, color:color2, nombre:name2} = perro2;
+console.log(edad2);
+console.log(color2);
+console.log(name2);
+
 
 
 //usando metodos
 perro.ladrar();
 
 perro2.dormir();
+
+
+//Aplicar desestructuracion en los parametros, hay que poner en llave los parametros {nombre, edad, color, amigable}
+//let registrarMascota=(nombre, edad, color, amigable) =>{
+let registrarMascota=({nombre, edad, color, amigable,peso}) =>{
+    console.log(`La mascota ${nombre} de ${edad} años y color ${color} es ${amigable} de ${peso}Kg`);
+}
+
+let nuevaMascota={
+    nombre:"Luky",
+    edad:3,
+    peso:18,
+    color:"Beagle",
+    amigable:"true"
+}
+
+//registrarMascota("Miky",15,"Negro/Balnco",true);
+registrarMascota(nuevaMascota);
