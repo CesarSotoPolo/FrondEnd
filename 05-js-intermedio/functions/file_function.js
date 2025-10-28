ddb226842ca7fe429705f93fb68958a5fc39f557
CesarSotoPolo <henrysoto10@gmail.com>
2025-10-27 23:04:33 -0500

    Cambio 271025-02

---
+++ 05-js-intermedio/functions/file_function.js
@@ -0,0 +1,17 @@
+const operacion = (a, b) => a * b;
+
+const cambiarAString = (num) =>{
+  return String(num)
+}
+
+const incluyeTexto = (txt1, txt2) => {
+  return txt1.includes(txt2)
+}
+
+export default operacion;
+
+export {
+  // operacion,
+  cambiarAString,
+  incluyeTexto
+}
